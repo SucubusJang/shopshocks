2ff6870b5ceacadfbd1682be0b9de391e87c962e
Your Name <you@example.com>
2021-03-24 18:11:03 +0700

    M

---
+++ rest.php
@@ -0,0 +1,14 @@
+<?php
+    include_once("dbcontrol.php");
+    include_once("util.php");
+    $debug_mode = false;
+    
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){ # select
+        debug_text("for GET Method" ,$debug_mode); 
+    }else if($_SERVER['REQUEST_METHOD'] == 'POST'){ # insert , update //ถ้ามีตัวแปรเข้ามาจะทำการเช็ค
+        debug_text("for POST Method", $debug_mode);
+    }else{
+        debug_text("Error Unknow this Request" ,$debug_mode);
+        http_response_code(405);
+    }
+?>
