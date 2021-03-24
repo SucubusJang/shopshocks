e9f9ea5600dd67b5994f9a1193a442f6f847bc53
Your Name <you@example.com>
2021-03-24 18:04:41 +0700

    M

--- DBcontroller.php
+++ DBcontroller.php
@@ -1,35 +1,22 @@
 <?php
 
 class db{
-    private $db;
-    private $debug;
-    function __construct($user, $pass, $dbname, $debug){ #query insert,update,delete,selcet || connect,disconncet
-        $this->debug = $debug;
-        $this->db = new mysqli("localhost",$user,$pass,$dbname);
-        $this ->db->set_charset("utf8");
-        //check conntion
-        if($this->db->connect_errno){
-            echo "Fail to connect to MYSQL: ".$this->db->connect_error;
-            exit();
-        }else{
-            $this->debug_text("Connect Sucess...");
-        }
-    } 
-    function debug_text($text){
-        if($this->debug){
-            // if true debug text on for track any
-            echo "Debug :{$text}<br>";
-        }
+    public $dbConn = null; // สร้างตัวแปรการเก็บ ค่า connect
+    public function connect() //เชื่อมต่อฐานข้อมูล
+    {
+        define("host", "localhost");
+        define("username", "root");
+        define("password", "");
+        define("database", "iti_store");
+        $this->dbConn = new mysqli(host, username, password, database);
+        if ($this->dbConn->connect_error) //แสดง error เมื่อเชิ่มต่อไม่ได้
+            die("Database Connection Error,Error No.:" .
+                $this->dbConn->connect_errno . " | " . $this->dbConn->connect->connect_error);
+        $this->dbConn->query("SET NAMES UTF8");
     }
-    function query($sql){
-        $result = $this->db->query($sql);
-        $this->debug_text($sql);
-        $data = $result->fetch_all(MYSQLI_ASSOC);
-        //$this->debug_text(print_r($data));
-        return $data;
-    }
-    function close(){
-        $this->db->close();
+    public function disconnect() //ตัดการเชื่อมต่อฐานข้อมูล
+    {
+        $this->dbConn->close();
     }
 }
 ?>
--- Register.php
+++ Register.php
@@ -1,4 +1,50 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+  
+</style>
+<body>
+    <div>
+        <div style="width: 500px; margin: 0 auto;border: 2px solid red; padding: 20px">
+        <h3>ShopShock Member Register</h3>
+            <form action="" method="post">
+                <table class="table" style="margin: 0 auto">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <label for="">Name:</label>
+                            </td>
+                            <td><input type="text" name="" id="name"></td>
+                        </tr>
+                        <tr>
+                            <td><label for="">NickName:</label></td>
+                            <td><input type="text" name="" id="nickname"></td>
+                        </tr>
+                        <tr>
+                            <td><label for="">Password:</label></td>
+                            <td><input type="text" name="" id="password"></td>
+                        </tr>
+                        <tr>
+                            <td><label for="">Confirm Password:</label></td>
+                            <td>
+                                <input type="text" name="" id="con_pass">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><input type="submit" value="Submit"> <input type="reset" value="Reset"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+    </div>
+</body>
 
-?>
+</html>
