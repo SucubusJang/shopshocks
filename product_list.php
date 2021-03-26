8d490b376de715eb78dde28dbb75c96a7a3d279d
kenk4282 <6306021422186@fitm.kmutnb.ac.th>
2021-03-26 11:26:32 +0700

    M

--- product_list.php
+++ product_list.php
@@ -9,27 +9,33 @@
 </head>
 
 <body onload="load_doc()">
-    <div id="out"></div>
+    <div>
+        <div id="out"></div>
+        <br>
+        <div id="out2"></div>
+    </div>
     <script>
         let arr;
-        lable = ['item_id', 'product code', 'product_name', 'brand', 'หน่วยนับ', 'ราคาขาย', 'instock', 'quantity'];
+        lable = ['item_id', 'product code', 'product_name', 'brand', 'หน่วยนับ', 'ราคาขาย', 'Stock_Quantity'];
 
         function load_doc() {
             let xhttp = new XMLHttpRequest();
+            out = document.getElementById("out");
             xhttp.onreadystatechange = function() {
                 console.log(this.readyState + ", ", this.status);
                 if (this.readyState == 4 && this.status == 200) {
                     arr = JSON.parse(this.responseText);
-                    out = document.getElementById("out");
+
                     text = "<table border='1'>";
                     for (i = 0; i < lable.length - 1; i++) {
                         text += "<th>" + lable[i] + "</th>";
                     }
                     text = "<tr>" + text + "</tr>";
                     for (i = 0; i < arr.length; i++) {
-                        for (j = 0; j < arr[i].length; j++) {
+                        for (j = 0; j < arr[i].length - 1; j++) {
                             text += "<td>" + arr[i][j] + "</td>";
                         }
+                        text += "<td>" + "<button onclick='sel_product(" + i + ")'> < ShopShock > </button>" + "</td>";
                         text = "<tr>" + text + "</tr>";
                     }
                     text += "</table>";
@@ -40,8 +46,19 @@
             xhttp.send();
         }
 
-        function tabele_data(arr) {
-
+        function sel_product(idx) {
+            out = document.getElementById("out2");
+            text = "<form>";
+            text += "<table border='1'>";
+            for (i = 0; i < lable.length - 1; i++) {
+                text += "<tr><td>" + lable[i] + "</td>";
+                text += "<td>" + arr[idx][i] + "</td></tr>";
+            }
+            text += "<tr><td>" + lable[6] + "</td>";
+            text += "<td><input type='number' name='' id='' min='1' max='" + arr[idx][6] + "'></td></tr>";
+            text += "<tr><td colspan='2'><button onclick='open_op()'>Add to Cart</button><input type='reset' value='Reset'></td></tr>";
+            text += "</table></form>";
+            out.innerHTML = text;
 
         }
     </script>
