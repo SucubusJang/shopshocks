79e6f0a901eb383b96a8214e74e89fb38899256c
Your Name <you@example.com>
2021-03-31 19:52:43 +0700

    M

--- product_rest.php
+++ product_rest.php
@@ -26,31 +26,31 @@
         $db = new database();
         $db->connect();
         $_bill[0][0] = 0;
+        $_first[0][0] = 0;
         $sql = ["firstbill"    =>"SELECT COUNT(`Bill_id`) count FROM `bill`",
                 "last_id"      =>"SELECT `Bill_id` FROM `bill` ORDER BY `Bill_id` DESC LIMIT 1",
                 "current_bill" =>"SELECT `Bill_id`,`Bill_Status` FROM bill WHERE `Cus_ID` = 1 ORDER by `Bill_id` DESC LIMIT 1",
                 "openbill"     =>"INSERT INTO `bill`(`Bill_id`, `Cus_ID`, `Bill_Status`) VALUES (1,'{$_SESSION['cus_id']}',0)",
                 "ins_pro"      =>"INSERT INTO `bill_detail`(`Bill_id`, `Product_ID`, `Quantity`, `Unit_Price`) 
                                   VALUES (1,'{$_POST['p_id']}','{$_POST['p_qty']}','{$_POST['p_price']}')",
-                "check_pro"    =>"SELECT COUNT(`Product_ID`) FROM `bill_detail` WHERE `Bill_id` = '{$_bill[0][0]}' AND `Product_ID` = '{$_POST['p_id']}'"
+                "check_pro"    =>"SELECT COUNT(`Product_ID`) FROM `bill_detail` WHERE `Bill_id` = '{$_bill[0][0]}' AND `Product_ID` = '{$_POST['p_id']}'",
+                "check_pro"    =>"UPDATE `bill_detail` SET `Quantity`='{$_POST['p_qty']}',`Unit_Price`='{$_POST['p_price']}' WHERE `Bill_id`='{$_bill[0][0]}' and `Product_ID`='{$_POST['p_id']}'"
                 ];
         $_first = $db->query($sql["firstbill"]);
         if($_first[0][0] == 0){
             $result = $db->exec($sql["openbill"]);
             $result = $db->exec($sql["ins_pro"]);
         }else{
-            $_bill = $db->query($sql["firstbill"]);
-            if($_bill[0][0] == 1){
-                 $check_pro = $db->query($sql["firstbill"]);
-                 if($check_pro[0][0] == 1){
+            $_bill = $db->query($sql["current_bill"]);
+            if($_bill[0][1] == 0){
+                 $check_pro = $db->query($sql["check_pro"]);
+                 if($check_pro[0][0] == 0){
                     $result = $db->exec($sql["ins_pro"]);
                  }
             }
         }
         $db->close();
-        // return $_bill;
-        // return $result;
-        return $check_pro;
+        return $result;
     }
 
 ?>
